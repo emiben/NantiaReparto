7c05acf6f24ade7e9208f6ef7efa17d2ca9b6a56
Emiliano <emiben1@hotmail.com>
2018-08-29 00:46:12 -0400

    Se comienza Venta UI

---
+++ app/src/main/java/com/nantia/repartonantia/venta/VentaActivity.java
@@ -0,0 +1,15 @@
+package com.nantia.repartonantia.venta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nantia.repartonantia.R;
+
+public class VentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venta);
+    }
+}
