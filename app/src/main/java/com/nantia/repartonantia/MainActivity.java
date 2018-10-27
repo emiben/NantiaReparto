505e9a272b097dcdabd57edce8fabbf860d5b63a
Emiliano <emiben1@hotmail.com>
2018-10-27 00:48:15 -0400

    Se agrega la View de Vendedores

--- app/src/main/java/com/nantia/repartonantia/MainActivity.java
+++ app/src/main/java/com/nantia/repartonantia/MainActivity.java
@@ -5,14 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
+
 import com.nantia.repartonantia.cliente.ClienteActivity;
 import com.nantia.repartonantia.listadeprecios.ListaDePreciosActivity;
 import com.nantia.repartonantia.map.MapActivity;
 import com.nantia.repartonantia.producto.ProductoActivity;
-import com.nantia.repartonantia.reparto.Reparto;
 import com.nantia.repartonantia.reparto.RepartoActivity;
 import com.nantia.repartonantia.stock.StockActivity;
+import com.nantia.repartonantia.usuario.VendedoresActivity;
 import com.nantia.repartonantia.venta.VentaActivity;
 
 import static com.nantia.repartonantia.utils.Constantes.KEY_VENTA_LISTA;
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 navigateToVentas();
                 break;
             case R.id.menu_vendedores_cv:
-//                startActivity(MapActivity.class);
+                startActivity(VendedoresActivity.class);
                 break;
             default:
                 break;
--- app/src/main/java/com/nantia/repartonantia/usuario/VendedoresActivity.java
+++ app/src/main/java/com/nantia/repartonantia/usuario/VendedoresActivity.java
@@ -2,8 +2,10 @@ package com.nantia.repartonantia.usuario;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.nantia.repartonantia.R;
+import com.nantia.repartonantia.data.DataHolder;
 
 public class VendedoresActivity extends AppCompatActivity {
 
@@ -11,5 +13,25 @@ public class VendedoresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendedores);
+
+        loadViewObjects();
+    }
+
+    private void loadViewObjects(){
+        if(DataHolder.getReparto() != null){
+            if(DataHolder.getReparto().getVendedor1() != null){
+                ((TextView)findViewById(R.id.vendedor1_nombre_tv))
+                        .setText(DataHolder.getReparto().getVendedor1().getNombreCompleto());
+                ((TextView)findViewById(R.id.vendedor1_caja_tv))
+                        .setText(String.valueOf(DataHolder.getReparto().getVendedor1().getSaldoCaja()));
+            }
+
+            if(DataHolder.getReparto().getVendedor2() != null){
+                ((TextView)findViewById(R.id.vendedor2_nombre_tv))
+                        .setText(DataHolder.getReparto().getVendedor2().getNombreCompleto());
+                ((TextView)findViewById(R.id.vendedor2_caja_tv))
+                        .setText(String.valueOf(DataHolder.getReparto().getVendedor2().getSaldoCaja()));
+            }
+        }
     }
 }
