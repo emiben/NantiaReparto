4481774d7f7def207baf32124681a012ce0a7604
Emiliano <emiben1@hotmail.com>
2018-10-26 23:28:36 -0400

    UI Vendedores comienzo....

---
+++ app/src/main/java/com/nantia/repartonantia/usuario/VendedoresActivity.java
@@ -0,0 +1,15 @@
+package com.nantia.repartonantia.usuario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nantia.repartonantia.R;
+
+public class VendedoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendedores);
+    }
+}
